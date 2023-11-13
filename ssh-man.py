798632dce9b78d2e6c25b96747004bd98e7e3262
Oha <oha@ohaa.xyz>
2023-11-13 17:05:48 +0100

    Added saving and fixed shit

--- ssh-man.py
+++ ssh-man.py
@@ -1,22 +1,48 @@
 import pathlib
 import os
+import pickle
+
+# Check if save file exists and create it if not
 savefiledir = f"{pathlib.Path.home()}/.config/ssh-man"
-saves = []
+savefile = f"{savefiledir}/saves.pkl"
+if os.path.exists(savefiledir):
+    if os.path.getsize(savefile) > 0:
+        with open(savefile, "rb") as f:
+            saves = pickle.load(f)
+    else:
+        saves = []  # create empty list
+else:
+    os.mkdir(savefiledir)  # create file and directory
+    os.mknod(savefile)
+    saves = []
+
 while True:
-    print("Welcome to Ohas shitty SSH-Man. What do you wanna do? \r\n [1] Add a new connection \r\n [2] View saved connections")
+    print("Welcome to Ohas shitty SSH-Man. What do you wanna do?\n [1] Add a new connection\n [2] View saved connections")
     sel = int(input())
+
     if sel == 1:
         name = input("Name of the connection: ")
         user = input("Your username: ")
         host = input("Hostname of the remote server: ")
         saves.append([name, user, host])
-        print(saves)
+        with open(savefile, "wb") as f:
+            pickle.dump(saves, f)
+
     elif sel == 2:
         a = 0
-        print("Saved connections: ")
-        for i in saves:
-            a = a +1
-            print(f"- [{a}] {i[0]}")
-        conn = int(input("Which connection would you like to connect to?"))
-        conn = conn - 1
-        os.system(f"konsole -e ssh {saves[conn][1]}@{saves[conn][2]}")
+        if not saves:
+            print("No saved connections.")
+        else:
+            print("Saved connections:")
+            for i in saves:
+                a = a + 1
+                print(f"- [{a}] {i[0]}")
+            conn = int(input("Which connection would you like to connect to?"))
+            conn -= 1
+            if 0 <= conn < len(saves):
+                os.system(f"konsole -e ssh {saves[conn][1]}@{saves[conn][2]}")
+            else:
+                print("Invalid connection number.")
+
+    else:
+        print("Invalid/Not implemented function.")
